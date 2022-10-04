2e27d79f4164d806cb94c1d64df8a459d1515060
nkzren <renan.nakazawa0@gmail.com>
2022-10-04 18:17:07 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.19 AS builder
+
+WORKDIR /opt/builder
+ADD wiki.go ./
+RUN ["go", "build", "wiki.go"]
+
+FROM ubuntu:jammy AS app
+WORKDIR /opt/wiki
+
+ADD ./view ./view
+COPY --from=builder /opt/builder/wiki ./
+
+RUN mkdir content && chown -R 1000:1000 content
+USER 1000
+
+ENTRYPOINT ["./wiki"]
---
+++ wiki.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := "content/" + p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "content/" + title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, err := template.ParseFiles("view/" + tmpl + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
